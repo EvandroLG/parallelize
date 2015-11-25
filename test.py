0fcfd10ecc14ca3a22d08283929d37b17c3c9fe0
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-11-25 16:31:38 -0200

    created gitignore

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase, mock
-import parallel_download as parallel, multiprocessing
+import parallelize, multiprocessing
 
 class TestParallelDownload(TestCase):
     def test_should_raise_a_exception_when_paramer_is_not_string_or_list(self):
-        self.assertRaises(TypeError, parallel.parallel_download, 1)
+        self.assertRaises(TypeError, parallelize.parallelize, 1)
 
-    @mock.patch('parallel_download._download')
+    @mock.patch('parallelize._download')
     def test_should_call_download_function_without_create_processes_when_parameter_is_a_string(self, *args):
-        parallel.parallel_download('http://www.google.com/')
-        self.assertEqual(parallel._download.call_count, 1)
+        parallelize.parallelize('http://www.google.com/')
+        self.assertEqual(parallelize._download.call_count, 1)
 
-    @mock.patch('parallel_download._download')
+    @mock.patch('parallelize._download')
     @mock.patch('multiprocessing.pool.Pool')
     def test_should_call_download_function_3_times(self, *args):
-        parallel.parallel_download([
+        parallelize.parallelize([
             'http://www.google.com/',
             'http://www.github.com/',
             'http://www.facebook.com/'
