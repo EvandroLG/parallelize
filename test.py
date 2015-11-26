275983f8692863b2e2d466538acef06a367bce22
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-11-26 08:32:32 -0200

    clear

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ class TestParallelDownload(TestCase):
     def test_should_call_download_function_without_create_processes_when_parameter_is_a_string(self, *args):
         pyrallelize.pyrallelize('http://www.google.com/')
         self.assertEqual(pyrallelize._download.call_count, 1)
+        pyrallelize._download.assert_called_with('http://www.google.com/', '')
 
     @mock.patch('pyrallelize._download')
     @mock.patch('multiprocessing.pool.Pool')
@@ -25,3 +26,9 @@ class TestParallelDownload(TestCase):
 
         self.assertEqual(multiprocessing.pool.Pool.call_count, 1)
         self.assertEqual(param, 3)
+
+    @mock.patch('pyrallelize._download')
+    def test_should_receive_receive_correct_value_passed_on_directory_parameter(self, *args):
+        pyrallelize.pyrallelize('http://www.google.com/', 'dir_test/')
+        self.assertEqual(pyrallelize._download.call_count, 1)
+        pyrallelize._download.assert_called_with('http://www.google.com/', 'dir_test/')
